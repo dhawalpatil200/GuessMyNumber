d9583d8fc393cfd62e4d425fed094546a30d4318
dhawalpatil200 <71254556+dhawalpatil200@users.noreply.github.com>
2022-02-08 18:09:18 +0530

    bug fix

--- script.js
+++ script.js
@@ -57,6 +57,7 @@ document.querySelector(".check").addEventListener("click", function () {
 document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
+  gameOn = true;
 
   // document.querySelector('.message').textContent = 'Start guessing...';
   displayMessage("Start guessing...");
